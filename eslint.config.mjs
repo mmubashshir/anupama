f295db4adbf994fca0a2c8371e1108a65bc09ee6
reevan leonal dsouza <ree1dsouza@gmail.com>
2025-05-05 01:45:23 +0530

    feat: configure tooling

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,6 +1,8 @@
-import { dirname } from "path";
-import { fileURLToPath } from "url";
-import { FlatCompat } from "@eslint/eslintrc";
+import { dirname } from 'path';
+import { fileURLToPath } from 'url';
+import { FlatCompat } from '@eslint/eslintrc';
+import eslintConfigPrettier from 'eslint-config-prettier/flat';
+import { defineConfig } from 'eslint/config';
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
@@ -9,8 +11,17 @@ const compat = new FlatCompat({
   baseDirectory: __dirname,
 });
 
-const eslintConfig = [
-  ...compat.extends("next/core-web-vitals", "next/typescript"),
-];
+const eslintConfig = defineConfig([
+  ...compat.config({
+    extends: [
+      'next',
+      'eslint-config-next',
+      'next/core-web-vitals',
+      'next/typescript',
+    ],
+  }),
+
+  eslintConfigPrettier,
+]);
 
 export default eslintConfig;
