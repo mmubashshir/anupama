275045048514608b6707f95faeaa82897e73a84c
Nigel <nperis27@gmail.com>
2025-05-07 16:37:26 +0530

    feat: add scope for seo

---
+++ globals.d.ts
@@ -0,0 +1,10 @@
+declare global {
+  namespace NodeJS {
+    interface ProcessEnv {
+      NEXT_PUBLIC_GOOGLE_ANALYTICS: string | undefined;
+      NEXT_PUBLIC_CLARITY_PROJECT_ID: string | undefined;
+    }
+  }
+}
+
+export { };
---
+++ src/env.ts
@@ -0,0 +1,14 @@
+import { createEnv } from '@t3-oss/env-nextjs';
+import { z } from 'zod';
+
+export const env = createEnv({
+  client: {
+    NEXT_PUBLIC_GOOGLE_ANALYTICS: z.string().optional(),
+    NEXT_PUBLIC_CLARITY_PROJECT_ID: z.string().optional(),
+  },
+
+  experimental__runtimeEnv: {
+    NEXT_PUBLIC_GOOGLE_ANALYTICS: process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS,
+    NEXT_PUBLIC_CLARITY_PROJECT_ID: process.env.NEXT_PUBLIC_CLARITY_PROJECT_ID,
+  },
+});
