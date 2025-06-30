551877e3a5fdf50152e03baff30cb4f8160d3909
reevan leonal dsouza <ree1dsouza@gmail.com>
2025-07-01 02:36:26 +0530

    feat: implement comment enhancement

--- src/components/comment.tsx
+++ src/components/comment.tsx
@@ -1,12 +1,8 @@
 'use client';
 
-import { createComment } from '~/app/actions/post-comment';
-
 import WPContentRenderer from '~/components/wp-content-renderer';
 
 import { getRelativeTime } from '~/utils/get-relative-time';
-
-import SubmitCommentButton from './submit-comment';
 
 interface CommentProps {
   post: {
@@ -39,6 +35,7 @@ export default function Comment({ post }: CommentProps) {
             {post.comments.nodes.map((comment, index) => (
               <div
                 key={comment.id}
+                data-commentid={String(comment.id)}
                 className={`pb-4 ${index !== (post.comments?.nodes.length ?? 0) - 1 ? 'border-b border-gray-100' : ''}`}
               >
                 <div className="mb-1 flex items-center justify-between">
@@ -64,43 +61,6 @@ export default function Comment({ post }: CommentProps) {
           </div>
         </div>
       ) : null}
-
-      {/* Leave a Comment */}
-      <div className="border-t border-gray-200 pt-8">
-        <h3 className="mb-6 text-xl font-bold">Leave A Comment</h3>
-
-        <form action={createComment} className="space-y-4">
-          <input
-            name="name"
-            type="text"
-            className="w-full border border-gray-300 p-3 text-sm focus:ring-1 focus:ring-red-400 focus:outline-none"
-            required
-            minLength={3}
-            placeholder="Your Name..."
-          />
-
-          <input
-            name="email"
-            type="email"
-            className="w-full border border-gray-300 p-3 text-sm focus:ring-1 focus:ring-red-400 focus:outline-none"
-            required
-            placeholder="Your Email..."
-          />
-
-          <textarea
-            name="content"
-            className="w-full resize-none border border-gray-300 p-3 text-sm focus:ring-1 focus:ring-red-400 focus:outline-none"
-            rows={5}
-            required
-            minLength={5}
-            placeholder="Type your comment here..."
-          />
-
-          <input name="postId" type="hidden" value={post.databaseId} />
-
-          <SubmitCommentButton />
-        </form>
-      </div>
     </div>
   );
 }
