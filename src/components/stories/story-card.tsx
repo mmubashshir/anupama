ca91d53b5ebc9ef648ca80fe5e70a2788e84fa64
Nigel <nperis27@gmail.com>
2025-07-04 12:58:42 +0530

    feat: optimize food carousel for mobile

--- src/components/stories/story-card.tsx
+++ src/components/stories/story-card.tsx
@@ -13,7 +13,7 @@ export interface StoryCardProps {
 
 function StoryCard({ image, category, headline, writerName }: StoryCardProps) {
   return (
-    <div className="group flex flex-col gap-8 border-b border-gray-200 bg-white pb-4 hover:cursor-pointer md:gap-0 md:border-0">
+    <div className="group flex flex-col border-b border-gray-200 bg-white pb-4 hover:cursor-pointer md:gap-0 md:border-0">
       {/* Image */}
       <div className="relative">
         <Image
@@ -25,8 +25,8 @@ function StoryCard({ image, category, headline, writerName }: StoryCardProps) {
         />
       </div>
 
-      <div className="z-10 ml-auto flex w-full flex-col gap-2 bg-white md:-mt-16 md:w-[90%] md:p-4 md:text-left">
-        <span className="text-base text-black">{category}</span>
+      <div className="z-10 ml-auto flex w-full flex-col bg-white md:-mt-16 md:w-[90%] md:p-4 md:text-left">
+        <span className="mt-2 text-sm text-black">{category}</span>
         <h2 className="mt-1 text-lg font-extrabold decoration-1 underline-offset-4 group-hover:underline md:text-2xl">
           {headline}
         </h2>
