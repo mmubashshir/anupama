927ef25e5115c6b66fcf0cb36873b53b7b37bd06
Nigel <nperis27@gmail.com>
2025-07-06 13:24:14 +0530

    fix: move preloading to layout

--- src/app/(home)/layout.tsx
+++ src/app/(home)/layout.tsx
@@ -1,10 +1,16 @@
-export default function HomeLayout({
+import PreloadPdf from '~/components/preload-pdf';
+
+import { getLatestMagazinePdfUrl } from '~/utils/get-latest-magazine';
+
+export default async function HomeLayout({
   children,
 }: {
   children: React.ReactNode;
 }) {
+  const pdfUrl = await getLatestMagazinePdfUrl();
   return (
     <>
+      <PreloadPdf href={pdfUrl} />
       <main>{children}</main>
     </>
   );
--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -3,22 +3,16 @@ import DailyNews from '~/components/daily-news';
 import Header from '~/components/header';
 import HealthAndMedicine from '~/components/health-medicine';
 import Hero from '~/components/hero';
-import PreloadPdf from '~/components/preload-pdf';
 import Social from '~/components/social';
 import Stories from '~/components/stories';
-
-import { getLatestMagazinePdfUrl } from '~/utils/get-latest-magazine';
 
 import VideoNews from '../video-news/page';
 
 export const revalidate = 60; // Revalidate every 1 minute
 
-export default async function Home() {
-  const pdfUrl = await getLatestMagazinePdfUrl();
-
+export default function Home() {
   return (
     <>
-      <PreloadPdf href={pdfUrl} />
       <Header />
       <Hero />
       <DailyNews />
