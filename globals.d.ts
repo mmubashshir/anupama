6b363fb600d1837e5515554700c810074f350bb0
reevan-manglore <ree1dsouza@gmail.com>
2025-08-30 14:15:43 +0530

    feat: update pnpm workspace and environment variables
    - Added new environment variables for Firebase configuration in src/env.ts
    - Updated pnpm-workspace.yaml to include additional built dependencies

--- globals.d.ts
+++ globals.d.ts
@@ -1,13 +1,21 @@
 declare global {
   namespace NodeJS {
     interface ProcessEnv {
-      NEXT_PUBLIC_GOOGLE_ANALYTICS: string | undefined;
-      NEXT_PUBLIC_CLARITY_PROJECT_ID: string | undefined;
       NEXT_PUBLIC_MODE: 'staging' | 'production' | undefined;
       NEXT_PUBLIC_WORDPRESS_BASE_URL: string | undefined;
-      NEXT_PUBLIC_ONESIGNAL_APP_ID: string | undefined;
+      NEXT_PUBLIC_SITE_URL: string | undefined;
+      NEXT_PUBLIC_GOOGLE_ANALYTICS: string | undefined;
+      NEXT_PUBLIC_CLARITY_PROJECT_ID: string | undefined;
+      NEXT_PUBLIC_FIREBASE_API_KEY: string | undefined;
+      NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN: string | undefined;
+      NEXT_PUBLIC_FIREBASE_PROJECT_ID: string | undefined;
+      NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID: string | undefined;
+      NEXT_PUBLIC_FIREBASE_APP_ID: string | undefined;
+      NEXT_PUBLIC_FIREBASE_VAPID_KEY: string | undefined;
+      NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET: string | undefined;
+      NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID: string | undefined;
     }
   }
 }
 
-export {};
+export { };
