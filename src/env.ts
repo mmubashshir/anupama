ad4670560b5818e4c686ac82d43632dfaed38acc
reevan-manglore <ree1dsouza@gmail.com>
2025-08-30 14:15:43 +0530

    feat: update pnpm workspace and environment variables
    - Added new environment variables for Firebase configuration in src/env.ts
    - Updated pnpm-workspace.yaml to include additional built dependencies

--- src/env.ts
+++ src/env.ts
@@ -3,18 +3,39 @@ import { z } from 'zod';
 
 export const env = createEnv({
   client: {
+    NEXT_PUBLIC_SITE_URL: z.string().optional(),
+    NEXT_PUBLIC_WORDPRESS_BASE_URL: z.string().optional(),
+    NEXT_PUBLIC_MODE: z.enum(['staging', 'production']).optional(),
     NEXT_PUBLIC_GOOGLE_ANALYTICS: z.string().optional(),
     NEXT_PUBLIC_CLARITY_PROJECT_ID: z.string().optional(),
-    NEXT_PUBLIC_MODE: z.enum(['staging', 'production']).optional(),
-    NEXT_PUBLIC_WORDPRESS_BASE_URL: z.string().optional(),
-    NEXT_PUBLIC_ONESIGNAL_APP_ID: z.string().optional(),
+    NEXT_PUBLIC_FIREBASE_API_KEY: z.string().optional(),
+    NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN: z.string().optional(),
+    NEXT_PUBLIC_FIREBASE_PROJECT_ID: z.string().optional(),
+    NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID: z.string().optional(),
+    NEXT_PUBLIC_FIREBASE_APP_ID: z.string().optional(),
+    NEXT_PUBLIC_FIREBASE_VAPID_KEY: z.string().optional(),
+    NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET: z.string().optional(),
+    NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID: z.string().optional(),
   },
 
   experimental__runtimeEnv: {
+    NEXT_PUBLIC_SITE_URL: process.env.NEXT_PUBLIC_SITE_URL,
+    NEXT_PUBLIC_WORDPRESS_BASE_URL: process.env.NEXT_PUBLIC_WORDPRESS_BASE_URL,
+    NEXT_PUBLIC_MODE: process.env.NEXT_PUBLIC_MODE,
     NEXT_PUBLIC_GOOGLE_ANALYTICS: process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS,
     NEXT_PUBLIC_CLARITY_PROJECT_ID: process.env.NEXT_PUBLIC_CLARITY_PROJECT_ID,
-    NEXT_PUBLIC_MODE: process.env.NEXT_PUBLIC_MODE,
-    NEXT_PUBLIC_WORDPRESS_BASE_URL: process.env.NEXT_PUBLIC_WORDPRESS_BASE_URL,
-    NEXT_PUBLIC_ONESIGNAL_APP_ID: process.env.NEXT_PUBLIC_ONESIGNAL_APP_ID,
+    NEXT_PUBLIC_FIREBASE_API_KEY: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+    NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN:
+      process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+    NEXT_PUBLIC_FIREBASE_PROJECT_ID:
+      process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+    NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID:
+      process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+    NEXT_PUBLIC_FIREBASE_APP_ID: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+    NEXT_PUBLIC_FIREBASE_VAPID_KEY: process.env.NEXT_PUBLIC_FIREBASE_VAPID_KEY,
+    NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET:
+      process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+    NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID:
+      process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID,
   },
 });
