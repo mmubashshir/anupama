d0b0ee9f1dec98360541afea8f5985703df0478b
reevan leonal dsouza <ree1dsouza@gmail.com>
2025-09-18 02:02:33 +0530

    feat: implement initial version of webhook to send push notification

--- src/app/api/push-notifications/route.ts
+++ src/app/api/push-notifications/route.ts
@@ -1,42 +1,72 @@
-import admin from 'firebase-admin';
-import { NextResponse } from 'next/server';
+import 'server-only';
 
-import { env } from '~/env';
+import { POST_CREATED_TOPIC } from '~/constants/firebase';
+import { NextResponse } from 'next/server';
+import * as z from 'zod';
+
+import { firebaseMessaging } from '~/utils/firebase-admin-utils';
+import { tryCatch } from '~/utils/try-catch';
 
 import type { Message } from 'firebase-admin/messaging';
 
+const webHookRequestSchema = z.object({
+  post_id: z.number(),
+  post: z.object({
+    ID: z.number(),
+    post_title: z.string(),
+  }),
+});
+
 export async function POST(request: Request): Promise<NextResponse> {
-  try {
-    const body = (await request.json()) as
-      | { url: string; firebaseToken: string }
-      | undefined;
+  const body = await request.json();
 
-    const payload: Message = {
-      notification: {
-        title: 'Hello world',
-        body: 'hELLO WORLD',
-      },
-      data: body?.url ? { url: body.url } : undefined,
-      topic: '<TOPIC_NAME>',
-    };
+  const parsedData = webHookRequestSchema.safeParse(body);
 
-    admin.initializeApp({
-      credential: admin.credential.cert({
-        projectId: process.env.FIREBASE_PROJECT_ID,
-        privateKey: process.env.FIREBASE_PRIVATE_KEY?.replace(/\\n/g, '\n'),
-        clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
-      }),
-    });
-
-    await admin.messaging().send(payload);
-
-    return NextResponse.json({ success: true });
-  } catch (error: any) {
-    console.error('Error sending notification:', error);
+  if (parsedData.error) {
+    console.error(
+      `Parsing of webhook request failed ${z.prettifyError(parsedData.error)}`,
+    );
 
     return NextResponse.json(
-      { error: error.message, details: error }, // Log full error details
+      {
+        error: z.prettifyError(parsedData.error),
+        details: z.treeifyError(parsedData.error),
+      },
+      { status: 400 },
+    );
+  }
+
+  const messagePayload: Message = {
+    topic: POST_CREATED_TOPIC,
+    notification: {
+      title: parsedData.data.post.post_title,
+      body: 'Some notification description',
+    },
+    data: { url: 'https://anupama.co.in/' },
+  };
+
+  const { data: _data, error } = await tryCatch(
+    firebaseMessaging.send(messagePayload),
+  );
+
+  if (error) {
+    console.error(
+      `Failed to send notification to topic ${POST_CREATED_TOPIC} failed with error ${JSON.stringify(error)}`,
+    );
+
+    return NextResponse.json(
+      { error: error.message, details: error },
       { status: 500 },
     );
   }
+
+  // eslint-disable-next-line no-console -- On sucessfull notification send logs are required
+  console.info(
+    `notification sent to topic ${POST_CREATED_TOPIC} with payload \n ${JSON.stringify(messagePayload)}`,
+  );
+
+  return NextResponse.json({
+    success: true,
+    message: `notification sent to topic ${POST_CREATED_TOPIC}`,
+  });
 }
