b211da39768a335831ad5c996aa8fb1f7377c778
reevan leonal dsouza <ree1dsouza@gmail.com>
2025-09-19 02:10:35 +0530

    feat: implement webhook protection mechanisam and implement notification handling

--- src/app/api/push-notifications/route.ts
+++ src/app/api/push-notifications/route.ts
@@ -1,11 +1,14 @@
 import 'server-only';
 
+import { env } from 'process';
 import { POST_CREATED_TOPIC } from '~/constants/firebase';
 import { NextResponse } from 'next/server';
 import * as z from 'zod';
 
 import { firebaseMessaging } from '~/utils/firebase-admin-utils';
 import { tryCatch } from '~/utils/try-catch';
+
+import { fetchLimitedPosts } from '~/services/posts';
 
 import type { Message } from 'firebase-admin/messaging';
 
@@ -14,11 +17,34 @@ const webHookRequestSchema = z.object({
   post: z.object({
     ID: z.number(),
     post_title: z.string(),
+    post_name: z.string(),
   }),
 });
 
+const WEBHOOK_AUTHENTICATION_HEADER = 'webhook-api-key';
+
 export async function POST(request: Request): Promise<NextResponse> {
-  const body = await request.json();
+  const authenticationToken = request.headers.get(
+    WEBHOOK_AUTHENTICATION_HEADER,
+  );
+
+  if (String(authenticationToken) !== String(env.WORDPRESS_WEBHOOK_API_KEY)) {
+    return NextResponse.json({ error: 'Key missing' }, { status: 401 });
+  }
+
+  const { data: body, error: bodyParsingError } = await tryCatch(
+    request.json(),
+  );
+
+  if (bodyParsingError) {
+    return NextResponse.json(
+      {
+        error: 'Invalid  payload recived',
+        details: bodyParsingError,
+      },
+      { status: 400 },
+    );
+  }
 
   const parsedData = webHookRequestSchema.safeParse(body);
 
@@ -36,13 +62,41 @@ export async function POST(request: Request): Promise<NextResponse> {
     );
   }
 
+  const { data: postData, error: postFetchingError } = await tryCatch(
+    fetchLimitedPosts({ filter: { id: parsedData.data.post_id } }),
+  );
+
+  if (postFetchingError) {
+    console.error(
+      `Error while fetching post  with id ${parsedData.data.post_id}  from wordpress  failed with error ${JSON.stringify(postFetchingError)}`,
+    );
+
+    return NextResponse.json(
+      { error: 'Error while fetching post from wordpress' },
+      { status: 500 },
+    );
+  }
+
+  const article = postData.posts?.nodes[0];
+
+  if (!article) {
+    console.error(`Post with post id ${parsedData.data.post_id} not found`);
+
+    return NextResponse.json(
+      { error: `Post with post id ${parsedData.data.post_id} not found` },
+      { status: 404 },
+    );
+  }
+
   const messagePayload: Message = {
     topic: POST_CREATED_TOPIC,
-    notification: {
-      title: parsedData.data.post.post_title,
-      body: 'Some notification description',
+    data: {
+      title: article.title ?? 'Notification title',
+      clickAction: `/${article.categories?.nodes[0].slug}/${article.slug}`,
+      imageUrl:
+        article.featuredImage?.node.sourceUrl ??
+        `${env.NEXT_PUBLIC_SITE_URL}/favicon-96x96.png`,
     },
-    data: { url: 'https://anupama.co.in/' },
   };
 
   const { data: _data, error } = await tryCatch(
@@ -62,7 +116,7 @@ export async function POST(request: Request): Promise<NextResponse> {
 
   // eslint-disable-next-line no-console -- On sucessfull notification send logs are required
   console.info(
-    `notification sent to topic ${POST_CREATED_TOPIC} with payload \n ${JSON.stringify(messagePayload)}`,
+    `notification sent to topic ${POST_CREATED_TOPIC} with url \n ${JSON.stringify(messagePayload.data)}`,
   );
 
   return NextResponse.json({
